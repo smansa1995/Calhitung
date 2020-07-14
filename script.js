c2bdca01ce3127d351b98bfc3ff216f56901a26d
smansa1995 <67179947+smansa1995@users.noreply.github.com>
2020-07-14 17:16:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,125 +1,166 @@
-const calculatorScreen = document.querySelector('.calculator-screen');
+const number = document.quarySelectorAll(".number")
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    console.log(event.target.value);
+  })
+})
+
+const calculatorScreen = document.quarySelector('.calculator-screen')
 
 const updateScreen = (number) => {
-    calculatorScreen.value = number
+  calculatorScreen.value = number
 }
 
-const numbers = document.querySelectorAll(".number");
-
-
-numbers.forEach((number) => {
-    number.addEventListener("click", (event) => {
-        console.log(event.target.value);
-    })
-});
-
-let prevNumber = '';
-let calculationOperator = '';
-let currentNumber = '0';
+const numbers = document.quarySelectorAll(".number")
 
 numbers.forEach((number) => {
-    number.addEventListener("click", (event) => {
-        inputNumber(event.target.value);
-        updateScreen(currentNumber);
-    })
+  number.addEventListener("click", (event) => {
+    updateScreen(event.target.value)
+  })
+})
+
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber = '0'
+
+/* delete */
+const inputNumber = (number) => {
+  currentNumber = number
+}
+/* delete */
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    inputNumber(event.target.value)
+    updateScreen(currentNumber)
+  })
 })
 
 const inputNumber = (number) => {
-    if (currentNumber === '0') {
-        currentNumber = number
-    } else {
-        currentNumber += number
-    }
+  if (currentNumber === '0') {
+    currentNumber = number
+  } else {
+    currentNumber += number
+  }
 }
 
-const operators = document.querySelectorAll(".operator")
+const operator = document.quarySelectorAll(".operator")
 
 operators.forEach((operator) => {
-    operator.addEventListener("click", (event) => {
-        inputOperator(event.target.value);
-    })
+  operator.addEventListener("click", (event) => {
+    console.log(event.target.value);
+  })
 })
 
 const inputOperator = (operator) => {
-    if (calculationOperator === '') {
-        prevNumber = currentNumber;
-    }
-    calculationOperator = operator;
-    currentNumber = '0';
+  prevNumber = currentNumber
+  calculationOperator = operator
+  currentNumber = ''
 }
 
-const equalSign = document.querySelector('.equal-sign');
+/*delete*/
+const operator = document.quarySelectorAll(".operator")
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    console.log(event.target.value);
+  })
+})
+/*delete*/
+
+/*delete*/
+const equalSign = document.quarySelector('.equal-sign')
 
 equalSign.addEventListener('click', () => {
-    calculate()
-    updateScreen(currentNumber)
+  console.log('equal button is pressed');
 })
+/*delete*/
 
 const calculate = () => {
-    let result = ''
-    switch (calculationOperator) {
-        case '+':
-            result = parseFloat(prevNumber) + parseFloat(currentNumber)
-            break
-        case '-':
-            result = prevNumber - currentNumber
-            break
-        case '*':
-            result = prevNumber * currentNumber
-            break
-        case '/':
-            result = prevNumber / currentNumber
-            break
-        default:
-            return
-
-    }
-
-
-    currentNumber = result;
-    calculationOperator = '';
+  let result = ''
+  switch (calculationOperator) {
+    case "+":
+      result = parseInt(prevNumber) + parseInt(currentNumber)
+      break;
+    case "-":
+      result = prevNumber - currentNumber
+      break;
+    case "*":
+      result = prevNumber * currentNumber
+      break;
+    case "/":
+      result = prevNumber / currentNumber
+      break;
+    default:
+      return
+  }
+  currentNumber = result
+  calculationOperator = ''
 }
 
-const clearBtn = document.querySelector('.all-clear');
+const equalSign = document.quarySelector('.equal-sign')
 
-clearBtn.addEventListener('click', () => {
-    clearAll();
-    updateScreen(currentNumber);
+equalSign.addEventListener('click', () => {
+  calculate()
+  updateScreen(currentNumber)
 })
 
+/*delete*/
+const clearBtn = document.quarySelector('.all-clear')
+
+clearBtn.addEventListener('click', () => {
+  console.log('AC button is pressed');
+})
+/*delete*/
+
 const clearAll = () => {
-    prevNumber = '';
-    calculationOperator = '';
-    currentNumber = '0';
+  prevNumber = ''
+  calculationOperator = ''
+  currentNumber = '0'
 }
 
-const decimal = document.querySelector('.decimal');
+const clearBtn = document.quarySelector('.all-clear')
+
+clearBtn.addEventListener('click', () => {
+  clearAll()
+  updateScreen(currentNumber)
+})
+
+/*delete*/
+const decimal = document.quarySelector('.decimal')
 
 decimal.addEventListener('click', (event) => {
-    inputDecimal(event.target.value);
-    updateScreen(currentNumber);
-});
+  console.log(event.target.value);
+})
+/*delete*/
 
-const inputDecimal = (dot) => {
-    if (currentNumber.includes('.')) {
-        return
-    }
-    currentNumber += dot;
+inputDecimal = (dot) => {
+  currentNumber += dot
 }
 
+const decimal = document.quarySelector('.decimal')
 
+decimal.addEventListener('click', (event) => {
+  inputDecimal(event.target.value)
+  updateScreen(currentNumber)
+})
 
-const persentage = document.querySelector('.percentage');
+case '+':
+  result - parseFloat(prevNumber) + parseFloat(currentNumber)
+  break;
 
-persentage.addEventListener('click', (event) => {
-    persentageNumber(event.target.value);
-    updateScreen(currentNumber);
-});
+inputDecimal = (dot) => {
+  if (currentNumber.includes('.')) {
+    return
+  }
+  currentNumber += dot
+}
 
-
-const persentageNumber = () => {
-    if (currentNumber === '0') {
-        return;
-    }
-    currentNumber = currentNumber / 100;
+const inputOperator = (operator) => {
+  if (calculationOperator === '') {
+    prevNumber = currentNumber
+  }
+  calculationOperator = operator
+  currentNumber = '0'
 }
